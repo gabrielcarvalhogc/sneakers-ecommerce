74741d5c9523d35973dff7080943325a269964bc
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-10-31 17:36:17 -0300

    feat: hamburger menu to small displays

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,20 +5,33 @@ import { CartIcon } from "../icons/icon-cart";
 import { Logo } from "./logo";
 import styles from "./header.module.scss"
 import { HeaderLinks } from "./headerLinks";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className={styles.header}>
       <nav className={styles.container}>
         <a href="/" data-testid="logo" className={styles.logo}>
-          <Logo/>
+          <Logo />
         </a>
-        <HeaderLinks/>
+        <div className={`${isOpen ? styles.hamburgerMenu : styles.open}`}>
+          <div className={styles.hamburgerIcon} onClick={toggleMenu}>
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+          </div>
+          <HeaderLinks />
+        </div>
       </nav>
       <div className={styles.userContainer}>
-        <button><CartIcon/></button>
-        <Image 
+        <button><CartIcon /></button>
+        <Image
           src="/images/image-avatar.png"
           width={40}
           height={40}
