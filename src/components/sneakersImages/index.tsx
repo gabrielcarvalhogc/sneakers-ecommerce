576d8134be3174eac1ed83e70e6b05068d20ddad
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-11-03 16:52:55 -0300

    refacto: add Image components as array map

--- src/components/sneakersImages/index.tsx
+++ src/components/sneakersImages/index.tsx
@@ -1,17 +1,22 @@
 "use client"
 
 import Image from "next/image";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import styles from "./sneakersImages.module.scss";
 
 export function SneakersImages() {
   const [sourceImage, setSourceImage] = useState(String);
-  const [isSelected, setIsSelected] = useState(false);
+  const [isSelectedClass, setIsSelectedClass] = useState<number | null>(null);
 
   const toggleImage = (index: number) => {
     const source = `/images/image-product-${index}.jpg`;
     setSourceImage(source);
-    setIsSelected(!isSelected)
+
+    if (isSelectedClass === index) {
+      setIsSelectedClass(null);
+    } else {
+      setIsSelectedClass(index);
+    }
   }
 
   return (
@@ -24,36 +29,17 @@ export function SneakersImages() {
         className={styles.imageProduct}
       />
       <div>
-        <Image
-          src="/images/image-product-1-thumbnail.jpg"
-          width={80}
-          height={80}
-          alt="Orange shoes"
-          onClick={() => toggleImage(1)}
-          className={isSelected ? styles.selected : ""}
-        />
-        <Image
-          src="/images/image-product-2-thumbnail.jpg"
-          width={80}
-          height={80}
-          alt="Orange shoes"
-          onClick={() => toggleImage(2)}
-          className={isSelected ? styles.selected : ""}
-        />
-        <Image
-          src="/images/image-product-3-thumbnail.jpg"
-          width={80}
-          height={80}
-          alt="Orange shoes"
-          onClick={() => toggleImage(3)}
-        />
-        <Image
-          src="/images/image-product-4-thumbnail.jpg"
-          width={80}
-          height={80}
-          alt="Orange shoes"
-          onClick={() => toggleImage(4)}
-        />
+        {[1,2,3,4].map((index: number) => (
+          <Image
+            key={index}
+            src={`/images/image-product-${index}-thumbnail.jpg`}
+            width={80}
+            height={80}
+            alt="Orange shoes"
+            onClick={() => toggleImage(index)}
+            className={isSelectedClass === index ? styles.selected : ""}
+          />
+        ))}
       </div>
     </div>
   )
