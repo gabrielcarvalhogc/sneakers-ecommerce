38892c1232b67b4135dfbf2e1dcb59d6281f6106
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-11-15 17:50:34 -0300

    fix: server value from useCart

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import React, { ReactNode, createContext, useContext, useState } from "react";
+import React, { ReactNode, createContext, useContext, useEffect, useState } from "react";
 
 interface CartContextProps {
   cartQuantity: number;
@@ -8,23 +6,27 @@ interface CartContextProps {
 }
 
 interface ChildrenProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const CartContext = createContext<CartContextProps | undefined>(undefined);
 
-export const CartProvider = ( {children}: ChildrenProps ) => {
-  const [cartQuantity, setCartQuantity] = useState(() => {
-    if(typeof window !== "undefined") {
-      const storedQuantity = localStorage.getItem("cartQuantity");
-      return storedQuantity ? parseInt(storedQuantity, 10) : 0;
-    }
-    return 0
-  });
+export const CartProvider = ({ children }: ChildrenProps) => {
+  const [cartQuantity, setCartQuantity] = useState(0);
+
+  useEffect(() => {
+    const isClient = typeof window !== "undefined";
+    const storedQuantity = isClient ? localStorage.getItem("cartQuantity") : null;
+    const initialCartQuantity = storedQuantity ? parseInt(storedQuantity, 10) : 0;
+
+    setCartQuantity(initialCartQuantity);
+  }, []);
 
   const addToCart = (quantity: number) => {
     setCartQuantity(() => {
-      localStorage.setItem("cartQuantity", quantity.toString());
+      if (typeof window !== "undefined") {
+        localStorage.setItem("cartQuantity", quantity.toString());
+      }
       return quantity;
     });
   };
@@ -39,8 +41,5 @@ export const CartProvider = ( {children}: ChildrenProps ) => {
 
 export const useCart = (): CartContextProps => {
   const context = useContext(CartContext);
-  if (!context) {
-    throw new Error("useCart must be used within a CartProvider");
-  }
-  return context;
+  return context || { cartQuantity: 0, addToCart: () => {} };
 };
